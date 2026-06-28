4617e437329e344a2dd1230c542bd79d85f9d438
wenndeymae <wenndeymae14@gmail.com>
2026-06-28 19:12:25 +0800

    Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Current Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last Modified
+document.getElementById("lastModified").textContent = document.lastModified;
